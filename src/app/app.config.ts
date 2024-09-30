0c7b6553ad933179339fe71e81a7a07968a0b639
TiFurt <tiagofurtadopfcarneiro@gmail.com>
2024-09-30 19:56:29 -0300

    configure firebase to host, deploy and emulators

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,36 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import {
-  provideRouter,
-  withComponentInputBinding
-} from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"employee-transport-2bd6a","appId":"1:902685995403:web:c4919dd6985672836b3f8e","storageBucket":"employee-transport-2bd6a.appspot.com","apiKey":"AIzaSyBVNXVyA6wlrrBep0GyDzOAJXbH7SlQjAY","authDomain":"employee-transport-2bd6a.firebaseapp.com","messagingSenderId":"902685995403","measurementId":"G-B1QL2GF9C7"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore()), providePerformance(() => getPerformance())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'employee-transport-2bd6a',
+        appId: '1:902685995403:web:c4919dd6985672836b3f8e',
+        storageBucket: 'employee-transport-2bd6a.appspot.com',
+        apiKey: 'AIzaSyBVNXVyA6wlrrBep0GyDzOAJXbH7SlQjAY',
+        authDomain: 'employee-transport-2bd6a.firebaseapp.com',
+        messagingSenderId: '902685995403',
+        measurementId: 'G-B1QL2GF9C7',
+      }),
+    ),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+  ],
 };
