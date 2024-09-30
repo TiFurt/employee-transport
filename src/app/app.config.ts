1dcc94cbb5a8afa290113a22dd43127e4540b331
TiFurt <tiagofurtadopfcarneiro@gmail.com>
2024-09-30 20:07:29 -0300

    configure environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { environment } from '@environments/environment';
 
 import { routes } from './app.routes';
 
@@ -17,17 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'employee-transport-2bd6a',
-        appId: '1:902685995403:web:c4919dd6985672836b3f8e',
-        storageBucket: 'employee-transport-2bd6a.appspot.com',
-        apiKey: 'AIzaSyBVNXVyA6wlrrBep0GyDzOAJXbH7SlQjAY',
-        authDomain: 'employee-transport-2bd6a.firebaseapp.com',
-        messagingSenderId: '902685995403',
-        measurementId: 'G-B1QL2GF9C7',
-      }),
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
     provideFirestore(() => getFirestore()),
