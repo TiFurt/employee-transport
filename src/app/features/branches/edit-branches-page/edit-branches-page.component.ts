e80b1a2de1fd77dc004227e9d55604005e599caa
thiagoc <thiagoc1415@gmail.com>
2024-10-01 20:36:06 -0300

    fix: fix edit autocomplete

--- src/app/features/branches/edit-branches-page/edit-branches-page.component.ts
+++ src/app/features/branches/edit-branches-page/edit-branches-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, effect, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
@@ -24,19 +24,30 @@ import { Branch } from '@app/core/models/branch.model';
   styleUrl: './edit-branches-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditBranchesPageComponent {
+export class EditBranchesPageComponent implements OnInit {
   private readonly router = inject(Router);
   private readonly formBuilder = inject(FormBuilder);
   private readonly branchesService = inject(BranchService);
   private readonly notificationService = inject(NotificationService);
   
-  id = input<string>();
-
   formGroup = this.formBuilder.group({
     name: ['', Validators.required],
     description: [''],
     location: [null as GeoPoint | null, Validators.required],
   });
+
+  initialPosition: google.maps.LatLngLiteral;
+
+  branchId = input<string>('', {alias: 'id'});
+
+  ngOnInit(): void {
+    if (!this.branchId()) {
+      this.router.navigate(['/branches']);
+      return;
+    }
+
+    this.getBranch();
+  }
 
   onMapClick = (event: google.maps.MapMouseEvent) => {
     if (!event.latLng) {
@@ -54,7 +65,7 @@ export class EditBranchesPageComponent {
     }
 
     const branch = new Branch({
-      id: this.id(),
+      id: this.branchId(),
       name: this.formGroup.value.name ?? '',
       description: this.formGroup.value.description ?? '',
       location: this.formGroup.value.location,
@@ -65,4 +76,15 @@ export class EditBranchesPageComponent {
       this.router.navigate(['/branches']);
     });
   };
+
+  private getBranch() {
+    const branchReff = this.branchesService.getRefById(this.branchId());
+    this.branchesService.getByRef(branchReff).subscribe((branch) => {
+      this.formGroup.patchValue(branch);
+      this.initialPosition = {
+        lat: branch.location?.latitude ?? 0,
+        lng: branch.location?.longitude ?? 0,
+      };
+    });
+  }
 }
