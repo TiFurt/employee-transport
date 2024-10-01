63a5822a56feb9d661762c6a930638da5080b2fa
Luis Felipe Lassarotte <luisfelassa@gmail.com>
2024-10-01 20:20:49 -0300

    feat: add delete btn de funcionario

--- src/app/features/employees/employee-card/employee-card.component.ts
+++ src/app/features/employees/employee-card/employee-card.component.ts
@@ -2,6 +2,7 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   Input,
   input,
 } from '@angular/core';
@@ -16,6 +17,11 @@ import {
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { Employee } from '@app/core/models/employee.model';
+import { filter } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { EmployeeService } from '../employee.service';
+import { ConfirmDialogComponent } from '@app/shared/confirm-dialog/confirm-dialog.component';
+import { NotificationService } from '@app/services/notification.service';
 
 @Component({
   selector: 'app-employee-card',
@@ -38,5 +44,32 @@ import { Employee } from '@app/core/models/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeCardComponent {
+  private readonly dialog = inject(MatDialog);
+  private readonly notificationService = inject(NotificationService);
+  private readonly employeeService = inject(EmployeeService);
   employee = input<Employee>();
+
+  delete(): void {
+    this.dialog
+      .open(ConfirmDialogComponent, {
+        data: {
+          title: 'Excluir Funcionario',
+          message: 'Deseja realmente excluir este funcionario?',
+        },
+      })
+      .afterClosed()
+      .pipe(filter(Boolean))
+      .subscribe(() => {
+        this.employeeService.delete(this.employee()!.id).subscribe({
+          next: () => {
+            this.notificationService.showSuccess('Pessoa excluída com sucesso');
+          },
+          error: (error: Error) => {
+            this.notificationService.showError(
+              `Erro ao excluir funcionario: ${error.message}`,
+            );
+          },
+        });
+      });
+  }
 }
