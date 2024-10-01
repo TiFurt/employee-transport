02824e0b5167f915c45a44f907aadaee2a6d5477
biel-correa <gabriel@correa.dev.br>
2024-10-01 19:52:32 -0300

    feature: add branch to employee

--- src/app/features/employees/create-employees-page/create-employees-page.component.ts
+++ src/app/features/employees/create-employees-page/create-employees-page.component.ts
@@ -1,3 +1,4 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GeoPoint } from '@angular/fire/firestore';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -6,10 +7,12 @@ import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { BranchService } from '@app/features/branches/services/branch.service';
 import { NotificationService } from '@app/services/notification.service';
 import { Employee } from '@features/employees/employee.model';
 import { EmployeeService } from '@features/employees/employee.service';
 import { BranchesMapComponent } from '@shared/branches-map/branches-map.component';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-employees-page',
@@ -18,10 +21,12 @@ import { BranchesMapComponent } from '@shared/branches-map/branches-map.componen
     MatFormField,
     MatLabel,
     MatInput,
+    MatSelectModule,
     ReactiveFormsModule,
     MatButton,
     MatIcon,
     BranchesMapComponent,
+    AsyncPipe,
   ],
   templateUrl: './create-employees-page.component.html',
   styleUrl: './create-employees-page.component.scss',
@@ -30,13 +35,17 @@ import { BranchesMapComponent } from '@shared/branches-map/branches-map.componen
 export class CreateEmployeesPageComponent {
   private readonly router = inject(Router);
   private readonly formBuilder = inject(FormBuilder);
+  private readonly branchService = inject(BranchService);
   private readonly employeesService = inject(EmployeeService);
   private readonly notificationService = inject(NotificationService);
 
   formGroup = this.formBuilder.group({
     name: ['', Validators.required],
     location: [null as GeoPoint | null, Validators.required],
+    branchId: ['', Validators.required],
   });
+
+  branches$ = this.branchService.getAll();
 
   onMapClick = (event: google.maps.MapMouseEvent) => {
     if (!event.latLng) {
@@ -56,6 +65,7 @@ export class CreateEmployeesPageComponent {
     const employee = new Employee({
       name: this.formGroup.value.name ?? '',
       location: this.formGroup.value.location ?? null,
+      branchId: this.formGroup.value.branchId ?? '',
     });
 
     this.employeesService.save(employee).subscribe(() => {
--- src/app/features/employees/employee.model.ts
+++ src/app/features/employees/employee.model.ts
@@ -4,6 +4,7 @@ import { Entity } from '@core/models/entity.model';
 export class Employee extends Entity {
   name: string;
   location: GeoPoint | null;
+  branchId: string;
 
   constructor(employee: Partial<Employee>) {
     super(employee);
