c1c64748a268c945b9067943ecbb56664e8de97e
thiagoc <thiagoc1415@gmail.com>
2024-09-30 23:54:02 -0300

    feat: add google maps integration and map in dashboard

---
+++ src/app/features/branches-map/branches-map.component.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+
+@Component({
+  selector: 'app-branches-map',
+  standalone: true,
+  imports: [GoogleMapsModule],
+  templateUrl: './branches-map.component.html',
+  styleUrl: './branches-map.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BranchesMapComponent {
+  zoom = 12; // Nível de zoom
+  center: google.maps.LatLngLiteral = { lat: -23.55052, lng: -46.633308 }; // Centro do mapa
+  options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: false,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+}
--- src/app/features/dashboard-page/dashboard-page.component.ts
+++ src/app/features/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BranchesMapComponent } from '../branches-map/branches-map.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>dashboard works!</p>`,
+  imports: [CommonModule, BranchesMapComponent],
+  templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
