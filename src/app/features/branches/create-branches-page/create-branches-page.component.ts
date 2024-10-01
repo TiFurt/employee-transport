c709fb342f30f9f0e191a9a65770221318348d37
biel-correa <gabriel@correa.dev.br>
2024-09-30 21:31:05 -0300

    feature: create form template

--- src/app/features/branches/create-branches-page/create-branches-page.component.ts
+++ src/app/features/branches/create-branches-page/create-branches-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-branches-page',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIcon, MatButton],
   templateUrl: './create-branches-page.component.html',
-  styleUrl: './create-branches-page.component.scss'
+  styleUrl: './create-branches-page.component.scss',
 })
-export class CreateBranchesPageComponent {
-
-}
+export class CreateBranchesPageComponent {}
