d053057884b577c920359f2120c22a0487157308
TiFurt <tiagofurtadopfcarneiro@gmail.com>
2024-09-30 22:23:36 -0300

    fix base service

--- src/app/core/services/base.service.ts
+++ src/app/core/services/base.service.ts
@@ -8,9 +8,9 @@ import {
   Firestore,
   query,
   updateDoc,
+  where,
 } from '@angular/fire/firestore';
 import { Entity } from '@core/models/entity.model';
-import { notDeletedWhereStatement } from '@core/services/not-deleted-query.helper';
 import { CollectionReference } from '@firebase/firestore';
 import { from, map, Observable } from 'rxjs';
 
@@ -18,11 +18,16 @@ export abstract class BaseService<T extends Entity> {
   protected readonly firestore: Firestore = inject(Firestore);
   protected abstract readonly collectionName: string;
   protected abstract readonly collection: CollectionReference<T>;
+  protected readonly notDeletedWhereStatement = where('deletedAt', '==', null);
+  protected readonly deletedWhereStatement = where('deletedAt', '!=', null);
 
   getAll(): Observable<T[]> {
-    return collectionData<T>(query(this.collection, notDeletedWhereStatement), {
-      idField: 'id',
-    });
+    return collectionData<T>(
+      query(this.collection, this.notDeletedWhereStatement),
+      {
+        idField: 'id',
+      },
+    );
   }
 
   getRefById(id: string): DocumentReference {
