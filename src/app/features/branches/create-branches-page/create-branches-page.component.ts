b206de7dfd929c91eecaff40ef717ef6995e5792
biel-correa <gabriel@correa.dev.br>
2024-09-30 21:37:41 -0300

    feature: create form group

--- src/app/features/branches/create-branches-page/create-branches-page.component.ts
+++ src/app/features/branches/create-branches-page/create-branches-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -6,8 +7,21 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-create-branches-page',
   standalone: true,
-  imports: [MatInputModule, MatIcon, MatButton],
+  imports: [MatInputModule, MatIcon, MatButton, ReactiveFormsModule],
   templateUrl: './create-branches-page.component.html',
   styleUrl: './create-branches-page.component.scss',
 })
-export class CreateBranchesPageComponent {}
+export class CreateBranchesPageComponent {
+  private readonly formBuilder = inject(FormBuilder);
+
+  formGroup = this.formBuilder.group({
+    name: ['', Validators.required],
+    description: [''],
+  });
+
+  save = () => {
+    if (this.formGroup.invalid) {
+      return;
+    }
+  };
+}
